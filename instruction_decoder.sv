58b2258e21e987bf9e2169bfaa7204feebb0c69d
Mark Belanger <mbelanger@olin.edu>
2025-11-06 16:14:08 -0500

    Add logic for determining if immed gen is needed

--- instruction_decoder.sv
+++ instruction_decoder.sv
@@ -1,23 +1,35 @@
+`include "instruction_types"
+
 module instruction_decoder #(
     
 ) (
     input logic clk,
     input logic[31:0] instruction,
-    output logic opcode,
-    output logic rs1,
-    output logic rs2,
-    output logic rd,
-    output logic funct7,
-    output logic funct3,
-    // output instructing type too?
+    output logic[6:0] opcode,
+    output logic[19:15] rs1,
+    output logic[24:20] rs2,
+    output logic[11:9] rd,
+    output logic[14:12] funct3,
+    output logic[30] funct7,
+    output logic need_immed
 );
 
-assign opcode = instruction[6:0];
-assign rs1 = instruction[19:15];
-assign rs2 = instruction[24:20];
-assign rd = instruction[11:9];
-assign funct7 = instruction[30];
-assign funct3 = instruction[14:12];
+    // Parse instruction
+    assign opcode = instruction[6:0];
+    assign rs1 = instruction[19:15];
+    assign rs2 = instruction[24:20];
+    assign rd = instruction[11:9];
+    assign funct7 = instruction[30];
+    assign funct3 = instruction[14:12];
+
+    // Determine if the immed gen is needed based off what instruction type it is
+    always_comb begin
+        if(opcode == I_TYPE || opcode == U_TYPE || opcode == J_TYPE)
+            need_immed = 1'b1;
+    end else begin
+        need_immed = 1'b0;
+    end
+    
 
 
 endmodule
