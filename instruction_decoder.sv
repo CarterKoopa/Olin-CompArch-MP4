f8da0424ed1c2e8d08c6b79d3b4253da8a23b515
Mark Belanger <mbelanger@olin.edu>
2025-11-11 13:42:27 -0500

    Add instruction type outputs

--- instruction_decoder.sv
+++ instruction_decoder.sv
@@ -3,15 +3,19 @@
 module instruction_decoder #(
     
 ) (
-    input logic clk,
     input logic[31:0] instruction,
     output logic[6:0] opcode,
-    output logic[19:15] rs1,
-    output logic[24:20] rs2,
-    output logic[11:9] rd,
-    output logic[14:12] funct3,
-    output logic[30] funct7,
-    output logic need_immed
+    output logic[4:0] rs1,
+    output logic[4:0] rs2,
+    output logic[4:0] rd,
+    output logic[2:0] funct3,
+    output logic[6:0] funct7,
+    output logic is_r_type,
+    output logic is_i_type,
+    output logic is_s_type,
+    output logic is_b_type,
+    output logic is_u_type,
+    output logic is_j_type
 );
 
     // Parse instruction
@@ -19,17 +23,17 @@ module instruction_decoder #(
     assign rs1 = instruction[19:15];
     assign rs2 = instruction[24:20];
     assign rd = instruction[11:9];
-    assign funct7 = instruction[30];
+    assign funct7 = instruction[31:25];
     assign funct3 = instruction[14:12];
 
-    // Determine if the immed gen is needed based off what instruction type it is
-    always_comb begin
-        if(opcode == I_TYPE || opcode == U_TYPE || opcode == J_TYPE)
-            need_immed = 1'b1;
-    end else begin
-        need_immed = 1'b0;
-    end
-    
+
+    // Determine instruction type
+    assign is_r_type = (opcode == R_TYPE);
+    assign is_i_type = (opcode == I_TYPE);
+    assign is_s_type = (opcode == S_TYPE);
+    assign is_b_type = (opcode == B_TYPE);
+    assign is_u_type = (opcode == U_TYPE);
+    assign is_j_type = (opcode == J_TYPE);
 
 
 endmodule
