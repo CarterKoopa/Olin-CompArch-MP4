30a43ff77c304454aafd877a7ff4589a80048b62
Mark Belanger <mbelanger@olin.edu>
2025-11-11 13:41:53 -0500

    Fix PC variable naming

--- program_counter.sv
+++ program_counter.sv
@@ -3,21 +3,13 @@ module program_counter #(
 ) (
     input clk,
     input logic increment_en,
-    input logic[31:0] current_pc
-    output logic[31:0] next_pc
+    input logic[31:0] next_pc,
+    output logic[31:0] current_pc
 );
-
-
-// I'm not sure it makes sense for this module to handle jumps and branch logic
-// With he architecture we are following, those operations seem external to
-// what the PC actually tracks. Like then this module would be responsible for 
-// decoding instruction bits and doing timing for when to write
-
-// just sequential logic: on posedge clock if increment_en, next_pc = current_pc?
 
     always_ff @(posedge clk) begin
         if(increment_en) begin
-            next_pc <= current_pc;
+            current_pc <= next_pc;
         end
     end
 
